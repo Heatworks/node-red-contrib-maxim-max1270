60b93c687942b5fab83f01d1caf8c86fff889a1b
Weston Catron <weston@heatworks.tech>
2017-11-15 11:36:07 -0500

    Update spi to work with adafruit library with bitbang capability.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node-red-contrib-maxim-max1270
 Node-RED node for communicating with a Maxium MAX1270 chip over SPI.
+
+# Installation
+
+Install https://github.com/adafruit/Adafruit_Python_GPIO.
+
+Navigate to your node red directory `cd ~/.node-red` then install node from npm `npm install @heatworks/node-red-contrib-adafruit-max31855`. Then restart node-red.
--- analog/spi_read.py
+++ analog/spi_read.py
@@ -1,9 +1,9 @@
 from time import time, sleep
 import math
-import spidev
 import sys
 
-spi = spidev.SpiDev()
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
 
 SPI_PORT = 0
 SPI_DEVICE = 1
@@ -14,6 +14,15 @@ if(len(sys.argv) > 3):
     SPI_DEVICE = int(sys.argv[2])
     CHANNEL = int(sys.argv[3])
 
+
+if spi is not None:
+    self._logger.debug('Using hardware SPI')
+    self._spi = spi
+elif clk is not None and cs is not None and do is not None:
+    self._logger.debug('Using software SPI')
+    # Default to platform GPIO if not provided.        
+    spi = SPI.BitBang(gpio, clk, None, do, cs)
+
 spi.open(SPI_PORT, SPI_DEVICE)
 
 SAMPLING_RATE = 1.000
@@ -21,6 +30,20 @@ REPORTING_RATE = 1.000
 if(len(sys.argv) > 5):
     SAMPLING_RATE = float(sys.argv[4]) / 1000
     REPORTING_RATE = float(sys.argv[5]) / 1000
+
+defaultSPI = true
+if (len(sys.argv) > 9):
+    SPI_CLK = int(sys.argv[6])
+    SPI_DI = int(sys.argv[7])
+    SPI_DO = int(sys.argv[8])
+    SPI_CS = int(sys.argv[9])
+    defaultSPI = false
+
+if defaultSPI:
+    spi = SPI.SpiDev(SPI_PORT, SPI_DEVICE)
+else:
+    gpio = GPIO.get_platform_gpio()
+    spi = SPI.BitBang(gpio, SPI_CLK, SPI_DI, SPI_DO, SPI_CS)
 
 values = []
 last_report = 0
@@ -42,7 +65,7 @@ def report():
 def readChannel(channel):
     command = 0b10000001
     command = command + (channel << 4)
-    results = spi.xfer([command, 0b0,0b0,0b0]);#//i
+    results = spi.transfer([command, 0b0,0b0,0b0]);#//i
     left = results[3] >> 7
     middle = results[2] << 1
     right = results[1] << 9;#8
